df0615c8c0262f932c1fc55acd0d91e9ae6b0802
Vishal Patil <vishalypa@example.com>
2025-07-04 18:46:31 +0530

    commits

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import { useContext, useState } from "react";
-import { AuthContext } from "../context/authContext";
+import { AuthContext } from "../context/AuthContext";
 
 export default function Register() {
   const { register } = useContext(AuthContext);
